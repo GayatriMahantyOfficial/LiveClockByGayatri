15d553ec474c5d5c75f226fa7b153c3cd4cf8398
Gayatri Mahanty <gayatrimahanty36@gmail.com>
2024-06-18 10:06:16 +0530

    Delete script10.js

--- script10.js
+++
@@ -1,4 +0,0 @@
-function getDateTime() {
-    var datetime = document.getElementById('datetime-picker').value;
-    document.getElementById('result').innerHTML = "Selected Date and Time: " + datetime;
-  }
